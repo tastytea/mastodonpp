ec3f753d6cfcb091df4134792edc0f30c6297682
tastytea <tastytea@tastytea.de>
2020-01-06 10:03:55 +0100

    2020-01-06

--- docs/menudata.js
+++ docs/menudata.js
@@ -54,4 +54,5 @@ var menudata={children:[
 {text:"Enumerations",url:"functions_enum.html"},
 {text:"Related Functions",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"File List",url:"files.html"}]},
+{text:"Examples",url:"examples.html"}]}
