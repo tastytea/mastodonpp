343fe2adca305954c3cb5ff37a6f1214c09599a3
tastytea <tastytea@tastytea.de>
2020-01-15 19:09:43 +0100

    Add unescape_html().

---
+++ include/helpers.hpp
@@ -0,0 +1,41 @@
+/*  This file is part of mastodonpp.
+ *  Copyright © 2020 tastytea <tastytea@tastytea.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MASTODONPP_HELPERS_HPP
+#define MASTODONPP_HELPERS_HPP
+
+#include <string>
+#include <string_view>
+
+namespace mastodonpp
+{
+
+using std::string;
+
+/*!
+ *  @brief  Replaces HTML entities with UTF-8 characters.
+ *
+ *  Supports named and numbered entities, decimal and hexadecimal.
+ *
+ *  @param  html The HTML to unescape.
+ *
+ *  @since  0.4.0
+ */
+const string unescape_html(string html);
+
+} // namespace mastodonpp
+
+#endif  // MASTODONPP_HELPERS_HPP
