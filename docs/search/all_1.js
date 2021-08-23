1b67fa0dc707fea1834d0c53bde4c195b04880f9
tastytea <tastytea@tastytea.de>
2021-08-23 23:48:11 +0200

    2021-08-23 # # Previous commits: gpg: Signatur vom 2020-11-27T15:05:17 CET gpg:                mittels DSA-Schlüssel 3555266864CA6D7FF45AA6E7CFC39497F1B26E07 gpg: Korrekte Signatur von "tastytea <tastytea@tastytea.de>" [ultimativ] gpg:                     alias "tastytea <gentoo@tastytea.de>" [ultimativ]

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['answer_5ftype_1',['answer_type',['../structmastodonpp_1_1answer__type.html',1,'mastodonpp']]],
-  ['api_2',['API',['../classmastodonpp_1_1API.html',1,'mastodonpp::API'],['../classmastodonpp_1_1API.html#af9fc7a7d152a851122f8acc3224d4047',1,'mastodonpp::API::API()']]]
+  ['api_2',['API',['../classmastodonpp_1_1API.html#af9fc7a7d152a851122f8acc3224d4047',1,'mastodonpp::API::API()'],['../classmastodonpp_1_1API.html',1,'mastodonpp::API']]]
 ];
--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['mastodonpp_20reference_34',['mastodonpp Reference',['../index.html',1,'']]],
-  ['make_5frequest_35',['make_request',['../classmastodonpp_1_1CURLWrapper.html#adc8808c3fad5397176f6755b09ee0be6',1,'mastodonpp::CURLWrapper']]],
-  ['mastodonpp_36',['mastodonpp',['../namespacemastodonpp.html',1,'']]]
+  ['make_5frequest_34',['make_request',['../classmastodonpp_1_1CURLWrapper.html#adc8808c3fad5397176f6755b09ee0be6',1,'mastodonpp::CURLWrapper']]],
+  ['mastodonpp_35',['mastodonpp',['../namespacemastodonpp.html',1,'']]],
+  ['mastodonpp_20reference_36',['mastodonpp Reference',['../index.html',1,'']]]
 ];
